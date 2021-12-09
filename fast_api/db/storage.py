49ee764eddd306c0718ffc0d447d7a1f362c390b
johanijbabaj <khaidukov.eugeniy@gmail.com>
2021-12-10 00:35:07 +0300

    Add working with AbstractStorage to person service instead of ElasticSearch

--- fast_api/db/storage.py
+++ fast_api/db/storage.py
@@ -3,6 +3,7 @@ from elasticsearch import AsyncElasticsearch
 from typing import Any, Optional, List
 from models.film import Film, FilmBrief
 from models.genre import Genre, GenreBrief
+from models.person import Person, PersonBrief
 
 from uuid import UUID
 
@@ -120,6 +121,63 @@ class ElasticStorage(AbstractStorage):
             GenreBrief(**genre.get("_source")) for genre in genres_info
         ]
         return genre_list
+
+    async def get_person(self, person_id: str) -> Optional[Person]:
+        """
+            Извлечь информацию о человеке из ElasticSearch по его строке
+            идентификатору
+        """
+        es_fields = ["id", "full_name", "birth_date", "films"]
+        doc = await self.__conn.get('persons', person_id, _source_includes=es_fields)
+        person_info = doc.get("_source")
+        # Спецификация API требует, чтобы поле идентификатора называлось UUID
+        person_info["uuid"] = person_info["id"]
+        person_info.pop("id")
+
+        return Person(**person_info)
+
+    async def get_persons(self,
+                          film_uuid: Optional[UUID],
+                          filter_name: Optional[str],
+                          sort: Optional[str],
+                          page_size: Optional[int],
+                          page_number: Optional[int]) -> List[PersonBrief]:
+        """
+            Получить список людей из ElasticSearch
+        """
+        page_number = page_number if page_number is not None else 1
+        page_size = page_size if page_size is not None else 9999
+        search_query = {
+            "from": (page_number - 1) * page_size,
+            "size": page_size,
+            "query": {"match_all": {}},
+            "sort": [
+                {sort or "full_name.raw": {"order": "asc"}}
+            ]
+        }
+        if film_uuid:
+            search_query['query'] = {
+                "match": {
+                    "films.id": str(film_uuid)
+                }
+            }
+        if filter_name:
+            search_query['query'] = {
+                "match": {
+                    "full_name": filter_name
+                }
+            }
+        es_fields = ["id", "full_name", "birth_date"]
+        doc = await self.__conn.search(
+            index='persons',
+            body=search_query,
+            _source_includes=es_fields
+        )
+        persons_info = doc.get("hits").get("hits")
+        person_list = [
+            PersonBrief(**person.get("_source")) for person in persons_info
+        ]
+        return person_list
 
 
 # Функция понадобится при внедрении зависимостей
--- fast_api/services/person.py
+++ fast_api/services/person.py
@@ -1,12 +1,13 @@
 import orjson
+
+from aioredis import Redis
 from functools import lru_cache
 from typing import List, Optional
 from uuid import UUID
 
-from aioredis import Redis
-from db.elastic import get_elastic
 from db.redis import get_redis
-from elasticsearch import AsyncElasticsearch
+from db.storage import AbstractStorage
+from db.storage import get_storage
 from fastapi import Depends
 from models.person import Person, PersonBrief
 
@@ -18,9 +19,9 @@ class PersonService:
         Сервис для получения информации о человеке по идентификатору
     """
 
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
+    def __init__(self, redis: Redis, storage: AbstractStorage):
         self.redis = redis
-        self.elastic = elastic
+        self.storage = storage
 
     async def get_by_id(self, person_id: str) -> Optional[Person]:
         """
@@ -29,7 +30,7 @@ class PersonService:
         person = await self._person_from_cache(person_id)
         if not person:
             # Если фильма нет в кеше, то ищем его в Elasticsearch
-            person = await self._get_person_from_elastic(person_id)
+            person = await self.storage.get_person(person_id)
             if not person:
                 # Если он отсутствует в Elasticsearch, значит, жанра вообще нет в базе
                 return None
@@ -38,19 +39,6 @@ class PersonService:
 
         return person
 
-    async def _get_person_from_elastic(self, person_id: str) -> Optional[Person]:
-        """
-            Извлечь информацию о человеке из ElasticSearch по его строке
-            идентификатору
-        """
-        es_fields = ["id", "full_name", "birth_date", "films"]
-        doc = await self.elastic.get('persons', person_id, _source_includes=es_fields)
-        person_info = doc.get("_source")
-        # Спецификация API требует, чтобы поле идентификатора называлось UUID
-        person_info["uuid"] = person_info["id"]
-        person_info.pop("id")
-
-        return Person(**person_info)
 
     async def _person_from_cache(self, person_id: str) -> Optional[Person]:
         """
@@ -80,54 +68,11 @@ class PersonService:
         """
         persons = await self._get_by_film_id_from_cache(film_uuid, filter_name, sort, page_size, page_number)
         if not persons:
-            persons = await self._get_by_film_id_from_elastic(film_uuid, filter_name, sort, page_size, page_number)
+            persons = await self.storage.get_persons(film_uuid, filter_name, sort, page_size, page_number)
             if not persons:
                 return []
             await self._put_films_to_cache(persons, film_uuid, filter_name, sort, page_size, page_number)
         return persons
-
-    async def _get_by_film_id_from_elastic(self,
-                                           film_uuid: Optional[UUID],
-                                           filter_name: Optional[str],
-                                           sort: Optional[str],
-                                           page_size: Optional[int],
-                                           page_number: Optional[int]) -> List[PersonBrief]:
-        """
-            Получить список людей из ElasticSearch
-        """
-        page_number = page_number if page_number is not None else 1
-        page_size = page_size if page_size is not None else 9999
-        search_query = {
-            "from": (page_number - 1) * page_size,
-            "size": page_size,
-            "query": {"match_all": {}},
-            "sort": [
-                {sort or "full_name.raw": {"order": "asc"}}
-            ]
-        }
-        if film_uuid:
-            search_query['query'] = {
-                "match": {
-                    "films.id": str(film_uuid)
-                }
-            }
-        if filter_name:
-            search_query['query'] = {
-                "match": {
-                    "full_name": filter_name
-                }
-            }
-        es_fields = ["id", "full_name", "birth_date"]
-        doc = await self.elastic.search(
-            index='persons',
-            body=search_query,
-            _source_includes=es_fields
-        )
-        persons_info = doc.get("hits").get("hits")
-        person_list = [
-            PersonBrief(**person.get("_source")) for person in persons_info
-        ]
-        return person_list
 
     async def _get_by_film_id_from_cache(self,
                                          film_uuid: Optional[UUID],
@@ -164,6 +109,6 @@ class PersonService:
 @lru_cache()
 def get_person_service(
         redis: Redis = Depends(get_redis),
-        elastic: AsyncElasticsearch = Depends(get_elastic),
+        storage: AbstractStorage = Depends(get_storage),
 ) -> PersonService:
-    return PersonService(redis, elastic)
+    return PersonService(redis, storage)
