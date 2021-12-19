4fc628c91a997a2e96ebade4e357bee4db0eea63
johanijbabaj <khaidukov.eugeniy@gmail.com>
2021-12-20 00:00:04 +0300

    pre-commit testing

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -4,15 +4,15 @@
 
 import json
 import os
+from http import HTTPStatus
 
 import aiohttp
 import pytest
 from elasticsearch import Elasticsearch, helpers
-from http import HTTPStatus
 
 # Строка с именем хоста и портом
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', 'localhost:9200')
-API_HOST = os.getenv('API_HOST', 'localhost:8000')
+ELASTIC_HOST = os.getenv("ELASTIC_HOST", "localhost:9200")
+API_HOST = os.getenv("API_HOST", "localhost:8000")
 
 
 @pytest.fixture()
@@ -23,35 +23,31 @@ def some_person(request):
     # Создаем схему индекса для поиска персон
     with open("testdata/schemes.json") as fd:
         schemes = json.load(fd)
-    scheme = schemes['person_scheme']
+    scheme = schemes["person_scheme"]
     docs = [
         {
             "id": "23d3d644-5abe-11ec-b50c-5378d698a87b",
             "full_name": "John Smith",
             "birth_date": "01.01.2001",
-            "films": [{"id": "6fbe525a-5abe-11ec-b50c-5378d698a87b", "title": "John's film", "role": "actor"}],
+            "films": [
+                {
+                    "id": "6fbe525a-5abe-11ec-b50c-5378d698a87b",
+                    "title": "John's film",
+                    "role": "actor",
+                }
+            ],
         }
     ]
     # Создаем поисковый индекс и заполняем документами
     es = Elasticsearch(f"http://{ELASTIC_HOST}")
-    es.indices.create('persons', scheme)
-    helpers.bulk(
-        es,
-        [
-            {
-                '_index': 'persons',
-                '_id': doc["id"],
-                **doc
-            }
-            for doc in docs
-        ]
-    )
+    es.indices.create("persons", scheme)
+    helpers.bulk(es, [{"_index": "persons", "_id": doc["id"], **doc} for doc in docs])
 
     def teardown():
         """Удалить созданные для тестирования временные объекты"""
         for doc in docs:
-            es.delete('persons', doc["id"])
-        es.indices.delete('persons')
+            es.delete("persons", doc["id"])
+        es.indices.delete("persons")
 
     request.addfinalizer(teardown)
 
@@ -64,19 +60,19 @@ def empty_index(request):
     # Создаем схему индекса для поиска персон
     with open("testdata/schemes.json") as fd:
         schemes = json.load(fd)
-    scheme = schemes['person_scheme']
+    scheme = schemes["person_scheme"]
     es = Elasticsearch(f"http://{ELASTIC_HOST}")
     # FIXME: Индекс может уже существовать из-за хвостов прошлых ошибок
     #        В рабочем варианте этого быть не должно, убрать потом
     try:
-        es.indices.delete('persons')
+        es.indices.delete("persons")
     except:
         pass
-    es.indices.create('persons', scheme)
+    es.indices.create("persons", scheme)
 
     def teardown():
         """Удалить созданные для тестирования временные объекты"""
-        es.indices.delete('persons')
+        es.indices.delete("persons")
 
     request.addfinalizer(teardown)
 
@@ -85,7 +81,9 @@ def empty_index(request):
 async def test_some_person(some_person):
     """Проверяем, что тестовый человек доступен по API"""
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"http://{API_HOST}/api/v1/person/23d3d644-5abe-11ec-b50c-5378d698a87b") as ans:
+        async with session.get(
+            f"http://{API_HOST}/api/v1/person/23d3d644-5abe-11ec-b50c-5378d698a87b"
+        ) as ans:
             assert ans.status == HTTPStatus.OK
             data = await ans.json()
             assert data["uuid"] == "23d3d644-5abe-11ec-b50c-5378d698a87b"
@@ -102,7 +100,7 @@ async def test_person_list(some_person):
             data = await ans.json()
             assert isinstance(data, list)
             assert len(data) == 1
-            assert data[0]['uuid'] == "23d3d644-5abe-11ec-b50c-5378d698a87b"
+            assert data[0]["uuid"] == "23d3d644-5abe-11ec-b50c-5378d698a87b"
             assert data[0]["full_name"] == "John Smith"
 
 
