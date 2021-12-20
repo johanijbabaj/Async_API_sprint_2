5ee58ef86ddd3bacec86e09f11ef3e6e29f15590
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2021-12-20 18:54:10 +0300

    тесты кэша

---
+++ tests/functional/src/test_film_cache.py
@@ -0,0 +1,34 @@
+"""
+Тесты для доступа к одиночному фильму по API
+"""
+
+import os
+
+import aiohttp
+import aioredis
+import pytest
+
+# Строка с именем хоста и портом
+ELASTIC_HOST = os.getenv('ELASTIC_HOST', 'localhost:9200')
+API_HOST = os.getenv('API_HOST', 'localhost:8000')
+
+
+@pytest.mark.asyncio
+async def test_some_film_cache(some_film):
+    """Проверяем, что тестовый фильм кэшируется после запроса по API"""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"http://{API_HOST}/api/v1/film/bb74a838-584e-11ec-9885-c13c488d29c0") as ans:
+            assert ans.status == 200
+            data = await ans.json()
+            assert data["uuid"] == "bb74a838-584e-11ec-9885-c13c488d29c0"
+            assert data["title"] == "Some film"
+            assert data["imdb_rating"] == 5.5
+    redis = await aioredis.create_redis_pool(
+        ('redis', 6379), maxsize=20, password='password'
+    )
+    cached = await redis.get("bb74a838-584e-11ec-9885-c13c488d29c0")
+    assert cached is not None
+    # AIORedis возвращает строку bytes с объектом в JSON формате. Проверяем,
+    # что в ней есть идентификатор и название фидьма
+    assert '"uuid":"bb74a838-584e-11ec-9885-c13c488d29c0"' in str(cached)
+    assert '"title":"Some film"' in str(cached)
