a03c44b56d7ff3c73a74033a23627ed1e53db7a5
johanijbabaj <khaidukov.eugeniy@gmail.com>
2021-12-22 06:25:44 +0300

    apply make_get_reuest fequest mixtures to search

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import os
 from dataclasses import dataclass
@@ -244,3 +245,11 @@ def make_get_request(session):
             )
 
     return inner
+
+
+@pytest.yield_fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
